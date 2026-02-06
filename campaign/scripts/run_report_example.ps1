2251494869b3dc64a0bb51c9b762b7d2276b38a1
valentinritz-coder <valentin.ritz@icloud.com>
2026-02-06 11:09:59 +0100

    Delete campaign/scripts directory

--- campaign/scripts/run_report_example.ps1
+++
@@ -1,7 +0,0 @@
-$RunDir = ".\campaign_runs\RUN_20260205_080000__morning_peak"
-$DeviceNdjson = ".\device_notifications.ndjson"
-
-python -m campaign.cli report `
-  --run-dir $RunDir `
-  --device-ndjson $DeviceNdjson `
-  --match-threshold 70
